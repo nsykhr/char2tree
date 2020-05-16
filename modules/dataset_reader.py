0910a7688c86d04d84e886cdcbf606c9e28e9f12
iftwigs <42752431+iftwigs@users.noreply.github.com>
2020-05-17 00:32:08 +0300

    Update dataset_reader.py

--- modules/dataset_reader.py
+++ modules/dataset_reader.py
@@ -1,6 +1,6 @@
 import re
 from overrides import overrides
-from typing import List, Dict, Iterator
+from typing import List, Tuple, Dict, Iterator
 
 from allennlp.data.instance import Instance
 from allennlp.data.tokenizers import Token, Tokenizer
@@ -8,27 +8,31 @@ from allennlp.data.dataset_readers.dataset_reader import DatasetReader
 from allennlp.data.token_indexers import SingleIdTokenIndexer, TokenIndexer
 from allennlp.data.fields import Field, TextField, SequenceLabelField, AdjacencyField
 
+ROOT_TOKEN = '<ROOT>'
 INTRATOKEN_TAG = 'app'
-
 
 @DatasetReader.register('ud_char_level')
 class UniversalDependenciesCharacterLevelDatasetReader(DatasetReader):
     def __init__(
             self,
             token_indexers: Dict[str, TokenIndexer] = None,
-            use_deptags: bool = False,
-            use_upos: bool = False,
+            use_char_level_segmentation: bool = True,
+            use_upos: bool = True,
             use_xpos: bool = False,
+            use_deptags: bool = True,
             tokenizer: Tokenizer = None,
+            root_token: str = ROOT_TOKEN,
             intratoken_tag: str = INTRATOKEN_TAG,
             lazy: bool = False
     ) -> None:
         super().__init__(lazy=lazy)
         self._token_indexers = token_indexers or {'tokens': SingleIdTokenIndexer()}
         self.use_xpos = use_xpos
+        self.use_char_level_segmentation = use_char_level_segmentation
         self.use_upos = use_upos
         self.use_deptags = use_deptags
         self.tokenizer = tokenizer
+        self.root_token = root_token
         self.intratoken_tag = intratoken_tag
 
     @staticmethod
@@ -41,90 +45,114 @@ class UniversalDependenciesCharacterLevelDatasetReader(DatasetReader):
                 for token in sentence.split('\n'):
                     if token.startswith('#') or not token:
                         continue
-                    output.append(token.split('\t'))
+                    if re.match('\t', token):
+                        output.append(token.split('\t'))
+                    else:
+                        output.append(token.split())
                 yield output
 
     def convert_one_sentence(self, lines: List[List[str]]) -> List[List[str]]:
-        # TODO: make columns 6 and 7 optional
 
-        token2char_id_mapping = {0: 0}
+        if self.use_char_level_segmentation:
 
-        moving_number = 1
-        for line in lines:
-            old_id = int(line[0])
-            new_id = moving_number + len(line[1]) - 1
-            token2char_id_mapping[old_id] = new_id
-            moving_number += len(line[1])
+            token2char_id_mapping = {0: 0}
 
-        new_lines = []
-        moving_number = 1
-        for line in lines:
-            
-            if line[6] != '_':
-                old_head_id = int(line[6])
+            moving_number = 1
+            for line in lines:
+                old_id = int(line[0])
+                new_id = moving_number + len(line[1]) - 1
+                token2char_id_mapping[old_id] = new_id
+                moving_number += len(line[1])
 
+            new_lines = [['0', self.root_token, self.root_token, 'ROOT', 'ROOT', '_', '0', 'ROOT', '_', '_']]
+            moving_number = 1
+            for line in lines:
                 for i, symbol in enumerate(line[1]):
                     new_line = line[:]
                     new_line[0] = str(moving_number)
-                    moving_number += 1
                     new_line[1] = symbol
 
-                    if i == len(line[1]) - 1:
-                        new_line[6] = str(token2char_id_mapping[old_head_id])
-                    else:
-                        new_line[6] = str(moving_number + 1)
-                        new_line[7] = self.intratoken_tag
+                    if line[6] != '_':
+                        old_head_id = int(line[6])
+
+                        if i == len(line[1]) - 1:
+                            new_line[6] = str(token2char_id_mapping[old_head_id])
+                        else:
+                            new_line[6] = str(moving_number + 1)
+                            new_line[7] = self.intratoken_tag
+
                     new_lines.append(new_line)
-                    
-            else:
-                
-                for i, symbol in enumerate(line[1]):
-                    new_line = line[:]
-                    new_line[0] = str(moving_number)
                     moving_number += 1
-                    new_line[1] = symbol
-                    
-                    new_lines.append(new_line)
+        else:
+            new_lines = [['0', self.root_token, self.root_token, 'ROOT', 'ROOT', '_', '0', 'ROOT', '_', '_']]
+
+            for line in lines:
+                new_lines.append(line)
 
         return new_lines
 
     @overrides
     def _read(self, file_path: str) -> Iterator[Instance]:
-        # TODO: make UPOS optional
-        for sentence in self.read_corpus(file_path):
-            converted_sentence = self.convert_one_sentence(sentence)
-        
-            chars = [x[1] for x in converted_sentence]
-            
-            if self.use_upos:
-                upos_tags = [x[3] for x in converted_sentence]
-            else:
-                upos_tags = None
+        if self.use_char_level_segmentation:
 
-            # arc_indices = [(int(x[0]), int(x[6])) for x in converted_sentence]
+            for sentence in self.read_corpus(file_path):
+                converted_sentence = self.convert_one_sentence(sentence)
 
-            if self.use_deptags:
-                arc_indices = [int(x[6]) for x in converted_sentence]
-                arc_tags = [x[7] for x in converted_sentence]
-            else:
-                arc_indices = None
-                arc_tags = None
+                chars = [x[1] for x in converted_sentence]
 
+                if self.use_upos:
+                    upos_tags = [x[3] for x in converted_sentence]
+                else:
+                    upos_tags = None
 
-            if self.use_xpos:
-                xpos_tags = [x[4] for x in converted_sentence]
-            else:
-                xpos_tags = None
+                if self.use_xpos:
+                    xpos_tags = [x[4] for x in converted_sentence]
+                else:
+                    xpos_tags = None
 
-            yield self.text_to_instance(chars, arc_indices=arc_indices, arc_tags=arc_tags, 
-                                        upos_tags=upos_tags, xpos_tags=xpos_tags)
+                if self.use_deptags:
+                    arc_indices = [(int(x[0]), int(x[6])) for x in converted_sentence if int(x[0]) > 0]
+                    arc_tags = [x[7] for x in converted_sentence if x[7] != 'ROOT']
+                else:
+                    arc_indices = None
+                    arc_tags = None
+
+                yield self.text_to_instance(chars, arc_indices=arc_indices, arc_tags=arc_tags,
+                                            upos_tags=upos_tags, xpos_tags=xpos_tags)
+        else:
+
+            corpus = self.read_corpus(file_path)
+            output = convert_one_sentence(corpus)
+
+            for x in output:
+
+                chars = [x[1] for x in output if len(x[0]) < 3]
+
+                if self.use_upos:
+                    upos_tags = [x[3] for x in output if len(x[0]) < 3]
+                else:
+                    upos_tags = None
+
+                if self.use_xpos:
+                    xpos_tags = [x[4] for x in output if len(x[0]) < 3]
+                else:
+                    xpos_tags = None
+
+                if self.use_deptags:
+                    arc_indices = [(x[0], x[6]) for x in output if x[0] != 0 and len(x[0]) < 3]
+                    arc_tags = [x[7] for x in output if len(x[0]) < 3 and x[7] != 'ROOT']
+                else:
+                    arc_indices = None
+                    arc_tags = None
+
+                yield self.text_to_instance(chars, arc_indices=arc_indices, arc_tags=arc_tags,
+                                            upos_tags=upos_tags, xpos_tags=xpos_tags)
 
     @overrides
     def text_to_instance(
             self,
             chars: List[str],
-            # arc_indices: List[Tuple[int, int]],
-            arc_indices: List[int] = None,
+            arc_indices: List[Tuple[int, int]] = None,
             arc_tags: List[str] = None,
             upos_tags: List[str] = None,
             xpos_tags: List[str] = None
@@ -140,13 +168,12 @@ class UniversalDependenciesCharacterLevelDatasetReader(DatasetReader):
 
         if upos_tags is not None:
             fields['upos_tags'] = SequenceLabelField(upos_tags, text_field, label_namespace='upos')
+
         if xpos_tags is not None:
             fields['xpos_tags'] = SequenceLabelField(xpos_tags, text_field, label_namespace='xpos')
 
-        if arc_indices is not None and arc_tags:
-            # fields['arc_tags'] = AdjacencyField(arc_indices, text_field, arc_tags, label_namespace='dependency')
-            fields['arc_tags'] = SequenceLabelField(arc_tags, text_field, label_namespace='dependency')
-            fields['arc_indices'] = SequenceLabelField(arc_indices, text_field, label_namespace='arc_indices')
+        if arc_indices is not None and arc_tags is not None:
+            fields['adjacency_matrix'] = AdjacencyField(arc_indices, text_field, arc_tags,
+                                                        label_namespace='dependency', padding_value=-1)
 
         return Instance(fields)
-
